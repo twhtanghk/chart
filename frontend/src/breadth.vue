1b3a384cb62df0be60e47bdc347fb8cd9d362d3b
root <twhtanghk@gmail.com>
2021-01-09 11:51:22 +0800

    add apexchart heatmap

--- frontend/src/breadth.vue
+++ frontend/src/breadth.vue
@@ -5,21 +5,37 @@
 <script lang='coffee'>
 {Sector} = require 'yahoo-stock'
 {Breadth} = require('./plugins/model.coffee').default
-Plotly = require 'plotly.js'
 {unpack} = require('./plugins/lib.coffee').default
+ApexCharts = require 'apexcharts'
+moment = require 'moment'
 
 export default
   name: 'breadth'
   methods:
     heatmap: ->
-      for name in Sector.list
+      sectors = for name in Sector.list
         name: name
         breadth: await Breadth.list data: id: name
-      layout =
-        xaxis:
-          type: 'category'
-          categoryorder: 'category ascending'
-      # Plotly.newPlot @$el, 
+      opts =
+        chart:
+          type: 'heatmap'
+        dataLabels:
+          enabled: false
+        plotOptions:
+          heatmap:
+            enableShades: true
+            shadeIntensity: 0.9
+            colorScale:
+              ranges: [
+                { from: 0, to: 30, color: '#FF0000', name: 'low' }
+                { from: 31, to: 100, color: '#1E8449', name: 'high'}
+              ]
+        series:
+          for {name, breadth} in sectors
+            name: name
+            data: {x: moment(date, "YYYY-MM-DD").format('YYYY-MM-DD'), y: percent} for {date, percent} in breadth
+      chart = new ApexCharts @$el, opts
+      chart.render()
   mounted: ->
     @heatmap()
 </script>
