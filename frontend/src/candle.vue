ec488f41c0af890903d896c882cbb21261057522
root <twhtanghk@gmail.com>
2021-01-16 22:36:18 +0800

    render chart for crypto currency

--- frontend/src/candle.vue
+++ frontend/src/candle.vue
@@ -24,56 +24,62 @@ export default
       switch type
         when 'stock'
           @data = await Stock.list data: id: id
-          series = [
-            {
-              name: 'candle'
-              type: 'candlestick'
-              data: {x: date, y: [open, high, low, close]} for {date, open, high, low, close} in @data
-            }
-            {
-              name: 'ema20'
-              type: 'line'
-              data: ma @data, 20, EMA.calculate
-            }
-            {
-              name: 'ema60'
-              type: 'line'
-              data: ma @data, 60, EMA.calculate
-            }
-            {
-              name: 'ema120'
-              type: 'line'
-              data: ma @data, 120, EMA.calculate
-            }
-            {
-              name: 'basePrice'
-              type: 'scatter'
-              data: [
-                {x: @data[20 - 1].date, y: @data[20 - 1].low, fillColor: 'yellow'}
-                {x: @data[60 - 1].date, y: @data[60 - 1].low, fillColor: 'yellow'}
-                {x: @data[120 - 1].date, y: @data[120 - 1].low, fillColor: 'yellow'}
-              ]
-            }
-          ]
-          @candle.updateOptions
-            xaxis:
-              type: 'datetime'
-              labels: 
-                format: 'yyyy-MM-dd'
-                formatter: (value, timestamp, opts) ->
-                  moment(timestamp).format 'YYYY-MM-DD'
-            yaxis:
-              decimalsInFloat: 2
-              min: =>
-                Math.min ...(low for {low} in @data)
-              max: =>
-                Math.max ...(high for {high} in @data)
-          @candle.updateSeries series
         when 'cryptoCurr'
-          @data = await CryptoCUrr.list data: id: id
+          @data = await CryptoCurr.list data: id: id
+      series = [
+        {
+          name: 'candle'
+          type: 'candlestick'
+          data: {x: date, y: [open, high, low, close]} for {date, open, high, low, close} in @data
+        }
+        {
+          name: 'ema20'
+          type: 'line'
+          data: ma @data, 20, EMA.calculate
+        }
+        {
+          name: 'ema60'
+          type: 'line'
+          data: ma @data, 60, EMA.calculate
+        }
+        {
+          name: 'ema120'
+          type: 'line'
+          data: ma @data, 120, EMA.calculate
+        }
+        {
+          name: 'basePrice'
+          type: 'scatter'
+          data: [
+            {x: @data[20 - 1].date, y: @data[20 - 1].low, fillColor: 'yellow'}
+            {x: @data[60 - 1].date, y: @data[60 - 1].low, fillColor: 'yellow'}
+            {x: @data[120 - 1].date, y: @data[120 - 1].low, fillColor: 'yellow'}
+          ]
+        }
+      ]
+      @candle.updateOptions
+        xaxis:
+          type: 'datetime'
+          labels: 
+            formatter: (value, timestamp, opts) ->
+              moment(timestamp).format 'YYYY-MM-DD'
+        yaxis:
+          decimalsInFloat: 2
+          min: =>
+            Math.min ...(low for {low} in @data)
+          max: =>
+            Math.max ...(high for {high} in @data)
+      @candle.updateSeries series
   mounted: ->
-    defaultTooltip = ({seriesIndex, dataPointIndex, w}) ->
-      w.globals.series[seriesIndex][dataPointIndex].toFixed 2
+    tooltip =
+      default: ({seriesIndex, dataPointIndex, w}) ->
+        w.globals.series[seriesIndex][dataPointIndex].toFixed 2
+      candle: ({seriesIndex, dataPointIndex, w}) ->
+        o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
+        h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
+        l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
+        c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
+        "open: #{o.toFixed(2)}, high: #{h.toFixed(2)}, low: #{l.toFixed(2)}, close: #{c.toFixed(2)}"
     @candle = new ApexCharts document.getElementById('candle'),
       chart:
         type: 'candlestick'
@@ -87,16 +93,11 @@ export default
       tooltip:
         shared: true
         custom: [
-          ({seriesIndex, dataPointIndex, w}) ->
-            o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
-            h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
-            l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
-            c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
-            "open: #{o.toFixed(2)}, high: #{h.toFixed(2)}, low: #{l.toFixed(2)}, close: #{c.toFixed(2)}"
-          defaultTooltip
-          defaultTooltip
-          defaultTooltip
-          defaultTooltip
+          tooltip.candle
+          tooltip.default
+          tooltip.default
+          tooltip.default
+          tooltip.default
         ]
       noData:
         text: 'Loading'
